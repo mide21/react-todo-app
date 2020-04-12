97b409d76c47af54970f472678f4d17c67ffdc8c
mide21 <63500063+mide21@users.noreply.github.com>
2020-04-13 10:25:12 +1300

    Add files via upload

---
+++ src/todo-list.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import FlipMove from 'react-flip-move';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+
+class TodoList extends React.Component {
+    render() {
+        let items = this.props.items;
+        const listItems = items.map(item => {
+            return <li key={item.key}>
+                {item.text}
+                <span
+                    onClick={() => this.props.handleDelete(item.key)}>
+                    <FontAwesomeIcon className="trash" icon={faTrashAlt} />
+                </span>
+            </li>
+        })
+        return (
+            <ul>
+                <FlipMove duration={300} easing="ease-in-out">
+                    {listItems}
+                </FlipMove>
+            </ul>
+        );
+    }
+}
+
+export default TodoList;
